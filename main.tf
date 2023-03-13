564f264c55f28a78c2a2aede9956f2eb0d591739
neetubaghel4567 <113992010+neetubaghel4567@users.noreply.github.com>
2023-03-13 12:15:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "nehatestvm11"
+  name         = "neetu"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
  project ="magnetic-quasar-378605"
